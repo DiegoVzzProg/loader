880fcd26b8476c56b5bf78dfb7faa690723c91d7
Diego Guadalupe <diego.avzz2004@gmail.com>
2024-08-21 21:37:51 -0600

    asd

--- modulos/reach.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-namespace reach {
-	extern bool enabled = false;
-	extern bool only_weapon = false;
-	extern bool requiere_sprint = false;
-	extern float distance = 3.f;
-	extern int hit_chance = 100;
-}
